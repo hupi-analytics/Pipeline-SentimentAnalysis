9907081cc2e31c965f20025c4c60a3e52dd8b2fd
Laffond Anthony <anthony.laffond@hupi.fr>
2016-08-08 15:04:50 +0200

    add some comments

--- Pipeline example.scala
+++ Pipeline example.scala
@@ -1,3 +1,5 @@
+// Sentiment Analysis
+
 // Package 
 import org.apache.spark.ml.feature._
 import org.apache.spark.ml.classification.LogisticRegression
@@ -15,37 +17,31 @@ val texte_clean = sc.textFile(path + "CommentairesLivres5000.csv").
   if(l.contains("|Positive|")) 1.0 else if (l.contains("|Negative|")) 0.0 else 3.0) )
   //map(l=>l._2).groupBy(l=>l).map(l=> (l._1,l._2.size)).take(10)
 
-//Equilibrage
-val texte_todelete = texte_clean.filter(l=> l._2==1.0).randomSplit(Array(0.7,0.3),seed=123)(0)
+// We choose around 2000 reviews with approximatly 50% positive and 50% negative.
+val texte_todelete = texte_clean.filter(l=> l._2==1.0).
+                                 randomSplit(Array(0.7,0.3),seed=123)(0)
 val texte = texte_clean.subtract(texte_todelete).persist()
 val n = texte.count()
 
-//toDF
+// Conversion in dataframe
 val texte_df = texte.toDF("text","label")
 texte_df.show(2)
 
-// Split data into training (80%) and test (20%) for DF
+// Split data into training (80%) and test (20%)
 val splits_df = texte_df.randomSplit(Array(0.8, 0.2), seed = 1234)
 val training_df = splits_df(0).cache()
 val test_df = splits_df(1)
 val n_training_df = training_df.count()
 val n_test_df = test_df.count()
+// Positive/Negative
+//training_df.map(l=>l(1)).groupBy(l=>l).map(l=> (l._1,l._2.size)).take(5)
 
-training_df.map(l=>l(1)).groupBy(l=>l).map(l=> (l._1,l._2.size)).take(5)
-
-
+// Split the text into Array
 val tokenizer = new Tokenizer().
   setInputCol("text").
   setOutputCol("words")
-  
-/*  
-val regexTokenizer = new RegexTokenizer().
-  setInputCol("text").
-  setOutputCol("words").
-  setPattern("\\W")
-  //setPattern("[^a-zA-Z0-9_âêîôûàéèùïöüäëÿÂÊÎÔÛÄËÏÖÜÀÆæÇÉÈŒœÙ]")
-*/
 
+// TF-IDF
 val hashingTF = new HashingTF().
   //setNumFeatures(1000).
   setInputCol(tokenizer.getOutputCol).
@@ -55,35 +51,39 @@ val idf = new IDF().
   setInputCol(hashingTF.getOutputCol).
   setOutputCol("features")
 
+// Logistic regression
 val lr = new LogisticRegression().
   setMaxIter(100).
   setRegParam(0.01)
 
+// Create our pipeline
 val pipeline = new Pipeline().setStages(Array(tokenizer, hashingTF, idf, lr))
 
+// Train the model
 val model = pipeline.fit(training_df)
 
+// Predict on the test dataset
 val test_pred = model.transform(test_df) //.select('text, 'label, 'prediction)
 
-// Taux d'erreurs :
-//général
+// Error rates :
+//general
 //test_pred.select('label,'prediction).map( l => l(0)==l(1)).groupBy(l=>l).map(l=>(l._1,l._2.size)).take(5)
-//positif :
+//positive
 //test_pred.select('label,'prediction).rdd.filter(l=>(l(0)==0)).map( l => l(0)==l(1)).groupBy(l=>l).map(l=>(l._1,l._2.size)).take(5)
-//négatif
+//négative
 //test_pred.select('label,'prediction).rdd.filter(l=>(l(0)==1)).map( l => l(0)==l(1)).groupBy(l=>l).map(l=>(l._1,l._2.size)).take(5)
 
-
-//Récupération
+// Saving the model
 model.save(path + "my_pipeline")
 
+// Load the model
 val model_save = PipelineModel.load(path + "my_pipeline")
 
+// Apply on test dataset (again)
 val test_pred_save = model_save.transform(test_df) //.select('text, 'label, 'prediction)
-
 //test_pred_save.select('label,'prediction).map( l => l(0)==l(1)).groupBy(l=>l).map(l=>(l._1,l._2.size)).take(3)
 
-//exemple de prédiction 
+// Some examples 
 val exemple = sc.parallelize( List( 
   ("je suis content",1.0) ,
   ("c'est nul", 0.0) ,
